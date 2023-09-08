516e2751450175bc49775ddeac1dc9904d462998
Martin-ReyDen <martin.leblancs@epitech.eu>
2023-09-08 16:23:05 +0800

    [ADD] Profile Page - Profile Avatar (we can select an image from photo gallery - User name - 4 Menus (Mes sauvetages, Mon compte, Mes documents, Préférences) - Deconnexion button [FIX] Error for Registration Page tests - Animations created errors when launching tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import RegistrationPage from './RegistrationPage/RegistrationPage'
 import LoginPage from './LoginPage/LoginPage'
+import ProfilePage from './Profile/ProfilePage'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -9,8 +10,10 @@ export default function App() {
   return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="LoginPage" screenOptions={{headerShown: false}}>
-          <Stack.Screen name="LoginPage" component={LoginPage} />
-          <Stack.Screen name="RegistrationPage" component={RegistrationPage} />
+            <Stack.Screen name="LoginPage" component={LoginPage} />
+            <Stack.Screen name="RegistrationPage" component={RegistrationPage} />
+            <Stack.Screen name="ProfilePage" component={ProfilePage} />
+
         </Stack.Navigator>
       </NavigationContainer>
   );
