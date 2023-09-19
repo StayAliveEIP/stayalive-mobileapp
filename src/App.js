c07b0b9eccb73725bc3287aa3e3328bf39ea8070
Noel Varga <noel.varga@epitech.eu>
2023-09-19 21:18:01 +0200

    [ADD] Creation of the "Available" page
    This page is composed of:
    - a profile logo button
    - Status indication (using text)
    - An image in the middle
    - A textbox with multiple pages
    - A button to change status

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import RegistrationPage from './RegistrationPage/RegistrationPage'
 import LoginPage from './LoginPage/LoginPage'
 import ProfilePage from './Profile/ProfilePage'
+import AvailablePage from './AvailablePage/AvailablePage'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IntroductionPage from './IntroductionPage/IntroductionPage';
@@ -11,10 +12,11 @@ const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName={'IntroductionPage'} screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName={'AvailablePage'} screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="LoginPage" component={LoginPage} />
                 <Stack.Screen name="RegistrationPage" component={RegistrationPage} />
                 <Stack.Screen name="ProfilePage" component={ProfilePage} />
+                <Stack.Screen name="AvailablePage" component={AvailablePage} />
                 <Stack.Screen name="IntroductionPage" component={IntroductionPage} />
             </Stack.Navigator>
         </NavigationContainer>
