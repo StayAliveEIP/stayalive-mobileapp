84ca28fe4ff6609a616395256e2b1c0d596027ae
Martin-ReyDen <martin.leblancs@epitech.eu>
2023-10-09 14:48:50 +0800

    [ADD] Request for Registration Page - The request is successfully working - We can successfully create an Account - An alert is displayed with the response message

--- src/RegistrationPage/RegistrationPage.js
+++ src/RegistrationPage/RegistrationPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, TouchableOpacity, Image, ScrollView} from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import {TextInputStayAlive} from './textInputStayAlive';
@@ -6,6 +6,9 @@ import LinearGradient from 'react-native-linear-gradient';
 import { colors } from '../Style/StayAliveStyle'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {SlideInView} from '../Animations/Animations'
+import Snackbar from 'react-native-snackbar';
+
+
 export default function RegistrationPage({ navigation }) {
     const [names, onChangeNames] = useState('');
     const [email, onChangeEmail] = useState('');
@@ -13,8 +16,43 @@ export default function RegistrationPage({ navigation }) {
     const [phone, onChangePhone] = useState('');
     const [selectCGUV, setSelectionCGUV] = useState(false);
 
-    const onClickJoinUs = () => {
-        console.log(names, email, password, phone, selectCGUV);
+    const onClickJoinUs = async () => {
+        let message = "";
+        let code = 0;
+        const namesSplited = names.split(' ');
+
+        if (selectCGUV !== false) {
+            try {
+                const requestBody = {
+                    email: email,
+                    firstname: namesSplited[1],
+                    lastname: namesSplited[0],
+                    password: password,
+                    phone: phone
+                };
+                const response = await fetch('http://api.stayalive.fr:3000/auth/register', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(requestBody),
+                });
+
+                const responseData = await response.json();
+                message = responseData.message;
+                code = response.status;
+            } catch (error) {
+                console.error('Erreur lors de la requête POST :', error);
+            }
+        }
+        else
+            message = "Vous devez acceptez nos CGU et nos CGV";
+        Snackbar.show({
+            text: message.toString(),
+            duration: 3000,
+            backgroundColor: "white",
+            textColor: code === 200 || code === 201 ? "green" : "red"
+        });
     };
 
     return (
