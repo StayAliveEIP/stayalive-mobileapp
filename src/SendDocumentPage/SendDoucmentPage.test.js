63f0ec60c7dc6e6a08fd27c8853530ef583c0114
Martin-ReyDen <martin.leblancs@epitech.eu>
2024-02-28 13:37:25 +0800

    [FIX] Test SEnd document due to navigation not mocked

--- src/SendDocumentPage/SendDoucmentPage.test.js
+++ src/SendDocumentPage/SendDoucmentPage.test.js
@@ -15,16 +15,23 @@ jest.mock('react-native-document-picker', () => ({
   },
 }))
 
+const mockNavigate = jest.fn()
+const mockNavigation = { navigate: mockNavigate, goBack: jest.fn() }
+
 describe('SendDocumentPage', () => {
   it('renders correctly', () => {
-    const { getByText, getByTestId } = render(<SendDocumentPage />)
+    const { getByText, getByTestId } = render(
+      <SendDocumentPage navigation={mockNavigation} />
+    )
     const logo = getByTestId('document-logo')
     expect(logo).toBeTruthy()
     expect(getByText('Envoyer mes documents')).toBeTruthy()
   })
 
   it('displays "Télécharger mon document" when no file is selected', () => {
-    const { getByTestId } = render(<SendDocumentPage />)
+    const { getByTestId } = render(
+      <SendDocumentPage navigation={mockNavigation} />
+    )
     const button = getByTestId('selectDocument-button-documentID')
     expect(button).toBeTruthy()
 
@@ -34,7 +41,9 @@ describe('SendDocumentPage', () => {
   })
 
   it('renders two BoxDocuments with specific ids', () => {
-    const { getByTestId } = render(<SendDocumentPage />)
+    const { getByTestId } = render(
+      <SendDocumentPage navigation={mockNavigation} />
+    )
 
     const documentIDBox = getByTestId('selectDocument-button-documentID')
     const documentSauveteurBox = getByTestId(
@@ -47,7 +56,9 @@ describe('SendDocumentPage', () => {
 
   it('clicks the left arrow button', () => {
     const consoleLogSpy = jest.spyOn(console, 'log')
-    const { getByTestId } = render(<SendDocumentPage />)
+    const { getByTestId } = render(
+      <SendDocumentPage navigation={mockNavigation} />
+    )
     const leftArrowButton = getByTestId('button-left-arrow')
     fireEvent.press(leftArrowButton)
     expect(consoleLogSpy).toHaveBeenCalledWith('arrow left clicked !')
@@ -55,7 +66,9 @@ describe('SendDocumentPage', () => {
   })
 
   it('renders the "Envoyer vos documents" button', () => {
-    const { getByText } = render(<SendDocumentPage />)
+    const { getByText } = render(
+      <SendDocumentPage navigation={mockNavigation} />
+    )
     const button = getByText('Envoyer vos documents')
     expect(button).toBeTruthy()
   })
