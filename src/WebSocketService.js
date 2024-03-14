cca3401a6e3e49920eb3070686846243fb98ccd8
Martin-ReyDen <martin.leblancs@epitech.eu>
2024-03-14 13:56:42 +0800

    [ADD] Notification with Notifee - Design of the notification - Permissions request (notification & battery usage -> can cause problems with the display of the notification) - Notification redirect to the AlertStatusPage

--- src/WebSocketService.js
+++ src/WebSocketService.js
@@ -1,8 +1,77 @@
+import { Alert } from 'react-native';
 import { io } from 'socket.io-client'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { urlApi } from '../src/Utils/Api'
-
+import notifee from '@notifee/react-native';
+import PropTypes from 'prop-types'
+import { colors } from '../src/Style/StayAliveStyle'
 let socket
+
+async function onDisplayNotification(navigation, dataAlert, token) {
+  const batteryOptimizationEnabled = await notifee.isBatteryOptimizationEnabled();
+  if (batteryOptimizationEnabled) {
+    Alert.alert(
+        'Restrictions Detected',
+        'To ensure notifications are delivered, please disable battery optimization for the app.',
+        [
+          {
+            text: 'OK, open settings',
+            onPress: async () => await notifee.openBatteryOptimizationSettings(),
+          },
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+        ],
+        { cancelable: false }
+    );
+  };
+  const powerManagerInfo = await notifee.getPowerManagerInfo();
+  if (powerManagerInfo.activity) {
+    Alert.alert(
+        'Restrictions Detected',
+        'To ensure notifications are delivered, please adjust your settings to prevent the app from being killed',
+        [
+          {
+            text: 'OK, open settings',
+            onPress: async () => await notifee.openPowerManagerSettings(),
+          },
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+        ],
+        { cancelable: false }
+    );
+  };
+  if (!dataAlert || !dataAlert.emergency || !dataAlert.emergency.info) {
+    console.error("Invalid dataAlert object.");
+    return;
+  }
+  const logoUri = require('../assets/StayAlive-logo.png');
+
+  const { info } = dataAlert.emergency;
+  const channelId = await notifee.createChannel({
+    id: 'default',
+    name: 'Emergency Channel',
+  });
+
+  await notifee.displayNotification({
+    title: '<b><font color="#E33A26">Vous avez une urgence</font></b>',
+    body: '<p><font color="#000000">' + info + '</font></p>',
+    android: {
+      smallIcon: 'ic_stayalive_logo',
+      color: colors.StayAliveRed,
+      channelId,
+      pressAction: {
+        id: 'default',
+      },
+    },
+  });
+}
+
 
 export const initializeWebSocket = async (navigation) => {
   try {
@@ -14,6 +83,7 @@ export const initializeWebSocket = async (navigation) => {
       socket.on('message', (data) => {
         const jsonData = data?.data
         if (jsonData !== null && jsonData !== undefined)
+          onDisplayNotification(navigation, jsonData, token);
           navigation.navigate('AlertStatusPage', { dataAlert: jsonData })
       })
 
