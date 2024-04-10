ea07af1fc88b3734ad556f2fdd4f331fe77a2433
Martin-ReyDen <martin.leblancs@epitech.eu>
2024-04-10 18:10:05 +0800

    [ADD] SendDocumentPage - Can send rescuer documents - Can delete rescuer documents - Can download rescuer documents

--- src/SendDocumentPage/SendDocumentPage.js
+++ src/SendDocumentPage/SendDocumentPage.js
@@ -1,201 +1,212 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import * as PropTypes from 'prop-types'
-import DocumentPicker from 'react-native-document-picker'
 import { colors } from '../Style/StayAliveStyle'
+import {BoxUploadDocument} from './BoxUploadDocument'
+import {BoxDocument} from './BoxDocument'
+import {urlApi} from "../Utils/Api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Snackbar from "react-native-snackbar";
 
-const imagePaths = {
-  documentID: require('../../assets/DocumentID.png'),
-  documentSauveteur: require('../../assets/DocumentSauveteur.png'),
-}
-
-export function BoxDocument(props) {
-  const { id, onFileSelect } = props
-  const [filename, setFilename] = useState(null)
-
-  SendDocumentPage.propTypes = {
-    navigation: PropTypes.object.isRequired,
-  }
-  const handleDocumentPick = async () => {
-    const result = await DocumentPicker.pick({
-      type: [DocumentPicker.types.allFiles],
-    })
-
-    const selectedFilename = result[0].name
-    setFilename(selectedFilename)
-    onFileSelect(id, result[0])
-  }
-
-  return (
-    <View
-      style={{
-        alignSelf: 'center',
-        alignItems: 'center',
-        backgroundColor: 'lightgray',
-        width: 350,
-        height: 200,
-        borderRadius: 20,
-        marginBottom: 30,
-      }}
-    >
-      <Image
-        style={{ marginTop: 10, width: 50, height: 50 }}
-        source={imagePaths[id]}
-      />
-      <Text
-        style={{
-          fontSize: 18,
-          fontWeight: 'bold',
-          color: 'black',
-          marginBottom: 3,
-        }}
-      >
-        {props.title}
-      </Text>
-      <Text style={{ fontSize: 16, color: 'black', textAlign: 'center' }}>
-        {props.description}
-      </Text>
-      <TouchableOpacity
-        onPress={handleDocumentPick}
-        style={{
-          marginTop: 14,
-          marginBottom: 10,
-          borderWidth: 3,
-          borderRadius: 50,
-          borderColor: colors.StayAliveRed,
-          paddingHorizontal: 50,
-          paddingVertical: 1,
-          backgroundColor: colors.StayAliveRed,
-        }}
-      >
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 16,
-            color: 'white',
-            fontWeight: 'bold',
-          }}
-          testID={`selectDocument-button-${id}`}
-        >
-          {filename
-            ? filename.length > 20
-              ? `${filename.slice(0, 20)}...`
-              : filename
-            : 'Télécharger mon document'}
-        </Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
-
-BoxDocument.propTypes = {
-  id: PropTypes.oneOf(Object.keys(imagePaths)),
-  description: PropTypes.string,
-  title: PropTypes.string,
-  onFileSelect: PropTypes.func,
-}
 
 export default function SendDocumentPage({ navigation }) {
-  const [selectedFiles, setSelectedFiles] = useState({
-    documentID: null,
-    documentSauveteur: null,
-  })
+    const [selectedFiles, setSelectedFiles] = useState({
+        ID_CARD : null,
+        RESCUER_CERTIFICATE: null,
+    })
+    const [documentID, setDocumentID] = useState(null);
+    const [documentRescuer, setDocumentRescuer] = useState(null);
 
-  const onFileSelect = (id, filename) => {
-    setSelectedFiles((prevSelectedFiles) => ({
-      ...prevSelectedFiles,
-      [id]: filename,
-    }))
-  }
 
-  const onPressSendDocuments = () => {
-    if (
-      selectedFiles.documentID !== null &&
-      selectedFiles.documentSauveteur !== null
-    ) {
-      console.log(
-        `Noms des fichiers sélectionnés : ${selectedFiles.documentID.name} ${selectedFiles.documentSauveteur.name}`
-      )
+    const onFileSelect = (id, filename) => {
+        setSelectedFiles((prevSelectedFiles) => ({
+            ...prevSelectedFiles,
+            [id]: filename,
+        }))
     }
-  }
 
-  const goBack = () => {
-    console.log('arrow left clicked !')
-    navigation.goBack()
-  }
+    const onPressSendDocuments = async (type) => {
+        try {
+            console.log("starting send documents");
+            const apiUrl = `${urlApi}/rescuer/document/upload?type=${type}`;
+            console.log(apiUrl);
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TouchableOpacity
-        testID="button-left-arrow"
-        style={{
-          position: 'absolute',
-          top: 20,
-          left: 20,
-          zIndex: 1,
-        }}
-        onPress={() => goBack()}
-      >
-        <Icon testID="document-logo" name="arrow-left" size={30} />
-      </TouchableOpacity>
+            const token = await AsyncStorage.getItem('userToken');
 
-      <View style={{ alignItems: 'center' }}>
-        <View style={{ alignItems: 'center' }}>
-          <Image
-            style={{ alignSelf: 'center', width: 120, height: 120 }}
-            source={require('../../assets/DocumentLogo.png')}
-          />
-          <Text
-            style={{
-              marginBottom: 40,
-              marginTop: 20,
-              fontSize: 30,
-              fontWeight: 'bold',
-              color: 'black',
-            }}
-          >
-            Envoyer mes documents
-          </Text>
+            const formData = new FormData();
+            formData.append('file', selectedFiles[type]);
+            console.log(formData)
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: formData,
+            });
+            console.log(response);
+            if (response.status === 201)
+                Snackbar.show({
+                text: 'Les documents on été envoyés avec succès',
+                duration: Snackbar.LENGTH_LONG,
+                backgroundColor: 'white',
+                textColor: 'green',
+            })
+            if (documentID === null)
+                setDocumentID(selectedFiles["ID_CARD"])
+            if (documentRescuer === null)
+                setDocumentRescuer(selectedFiles["RESCUER_CERTIFICATE"])
+        } catch (error) {
+            Snackbar.show({
+                text: 'Une erreur est apparue lors de l\'envoie des documents',
+                duration: Snackbar.LENGTH_LONG,
+                backgroundColor: 'white',
+                textColor: 'green',
+            })
+            console.error('Erreur lors de l\'envoi des documents : ', error);
+        }
+    };
+
+
+    const goBack = () => {
+        console.log('arrow left clicked !')
+        navigation.goBack()
+    }
+
+    const getDocuments = async () => {
+        try {
+            const apiUrl = `${urlApi}/rescuer/document/all`;
+            const token = await AsyncStorage.getItem('userToken')
+
+            const response = await fetch(apiUrl, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                }});
+
+
+            const responseData = await response.json();
+            console.log("get document ")
+            console.log(responseData)
+
+            const dataOfIdCard = responseData.find(item => item.type === "ID_CARD");
+            const dataOfRescuerCertificate = responseData.find(item => item.type === "RESCUER_CERTIFICATE");
+            if (dataOfIdCard.data !== null)
+                setDocumentID(dataOfIdCard.data);
+            if (dataOfRescuerCertificate.data !== null)
+                setDocumentRescuer(dataOfRescuerCertificate.data);
+        } catch (error) {
+            Snackbar.show({
+                text: 'Une erreur est apparue lors de la récupération des documents',
+                duration: Snackbar.LENGTH_LONG,
+                backgroundColor: 'white',
+                textColor: 'green',
+            })
+            console.error('Erreur lors de la récupération des documents : ', error);
+        }
+    };
+
+    useEffect(() => {
+        getDocuments();
+    }, []);
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <TouchableOpacity
+                testID="button-left-arrow"
+                style={{
+                    position: 'absolute',
+                    top: 20,
+                    left: 20,
+                    zIndex: 1,
+                }}
+                onPress={() => goBack()}
+            >
+                <Icon testID="document-logo" name="arrow-left" size={30} />
+            </TouchableOpacity>
+
+            <View style={{ alignItems: 'center' }}>
+                <View style={{ alignItems: 'center' }}>
+                    <Image
+                        style={{ alignSelf: 'center', width: 120, height: 120 }}
+                        source={require('../../assets/DocumentLogo.png')}
+                    />
+                    <Text
+                        style={{
+                            marginBottom: 40,
+                            marginTop: 20,
+                            fontSize: 30,
+                            fontWeight: 'bold',
+                            color: 'black',
+                        }}
+                    >
+                        Envoyer mes documents
+                    </Text>
+                </View>
+                {documentID === null ? (
+                        <BoxUploadDocument
+                            onFileSelect={onFileSelect}
+                            id="ID_CARD"
+                            title="Pièce d'identité"
+                            description="Nous avons besoin de ce document afin de vous identifier auprès des services de secours et en cas de besoin, pour vous contacter."
+                        />) :
+                    (<BoxDocument
+                            id="documentID"
+                            type={"ID_CARD"}
+                            title={"Pièce d'identité"}
+                            data={documentID}
+                            setData={setDocumentID}
+                        />
+                    )}
+                {documentRescuer === null ? (
+                        <BoxUploadDocument
+                            onFileSelect={onFileSelect}
+                            id="RESCUER_CERTIFICATE"
+                            title="Certificat de secourisme"
+                            description="Nous avons besoin de ce document afin de vous identifier auprès des services de secours et en cas de besoin, pour vous contacter."
+                        />) :
+                    (<BoxDocument
+                        id="documentSauveteur"
+                        type="RESCUER_CERTIFICATE"
+                        title="Certificat de secourisme"
+                        data={documentRescuer}
+                        setData={setDocumentRescuer}/>)}
+                {documentID !== null && documentRescuer !== null ? null : (
+                <TouchableOpacity
+                    onPress={() => {
+                        console.log(selectedFiles);
+                        if (selectedFiles["ID_CARD"] !== null) {
+                            onPressSendDocuments("ID_CARD");
+                        }
+                        if (selectedFiles["RESCUER_CERTIFICATE"] !== null) {
+                            onPressSendDocuments("RESCUER_CERTIFICATE");
+                        }
+                    }}
+                    style={{
+                        marginTop: 20,
+                        borderWidth: 3,
+                        borderRadius: 50,
+                        borderColor: colors.StayAliveRed,
+                        paddingHorizontal: 50,
+                        paddingVertical: 10,
+                        backgroundColor: 'white',
+                    }}
+                    testID="sendDocument-button"
+                >
+                    <Text
+                        style={{
+                            textAlign: 'center',
+                            fontSize: 18,
+                            color: colors.StayAliveRed,
+                            fontWeight: 'bold',
+                        }}
+                    >
+                        Envoyer vos documents
+                    </Text>
+                </TouchableOpacity>)}
+
+            </View>
         </View>
-
-        <BoxDocument
-          onFileSelect={onFileSelect}
-          id="documentID"
-          title={"Pièce d'identité"}
-          description="Nous avons besoin de ce document afin de vous identifier auprès des services de secours et en cas de besoin, pour vous contacter."
-        />
-        <BoxDocument
-          onFileSelect={onFileSelect}
-          id="documentSauveteur"
-          title="Certificat de secourisme"
-          description="Nous avons besoin de ce document afin de vous identifier auprès des services de secours et en cas de besoin, pour vous contacter."
-        />
-        <TouchableOpacity
-          onPress={onPressSendDocuments}
-          style={{
-            marginTop: 20,
-            borderWidth: 3,
-            borderRadius: 50,
-            borderColor: colors.StayAliveRed,
-            paddingHorizontal: 50,
-            paddingVertical: 10,
-            backgroundColor: 'white',
-          }}
-          testID="sendDocument-button"
-        >
-          <Text
-            style={{
-              textAlign: 'center',
-              fontSize: 18,
-              color: colors.StayAliveRed,
-              fontWeight: 'bold',
-            }}
-          >
-            Envoyer vos documents
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  )
+    )
 }
