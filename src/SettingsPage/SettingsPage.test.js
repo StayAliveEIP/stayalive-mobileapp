69321befb3df6a6e89bff85f6a039c451e862a9a
Martin-ReyDen <martin.leblancs@epitech.eu>
2024-04-23 18:24:21 +0800

    [FIX] Linter error

--- src/SettingsPage/SettingsPage.test.js
+++ src/SettingsPage/SettingsPage.test.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import SettingsPage from './SettingsPage';
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react-native'
+import SettingsPage from './SettingsPage'
 
 describe('SettingsPage', () => {
   test('should call navigation.goBack when left arrow button is pressed', () => {
     const navigationMock = {
       goBack: jest.fn(),
-    };
+    }
 
-    const { getByTestId } = render(<SettingsPage navigation={navigationMock} />);
+    const { getByTestId } = render(<SettingsPage navigation={navigationMock} />)
 
-    const leftArrowButton = getByTestId('button-left-arrow');
-    fireEvent.press(leftArrowButton);
+    const leftArrowButton = getByTestId('button-left-arrow')
+    fireEvent.press(leftArrowButton)
 
-    expect(navigationMock.goBack).toHaveBeenCalled();
-  });
+    expect(navigationMock.goBack).toHaveBeenCalled()
+  })
 
   // Add more tests as needed
-});
+})
