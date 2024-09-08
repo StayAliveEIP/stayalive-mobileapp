2537318bd23e4b6b221224582b0d0c7ad8dade43
Martin-ReyDen <martin.leblancs@epitech.eu>
2024-09-08 19:50:44 +0200

    [FIX] Prettier linter error

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -1,3 +1,4 @@
 export const urlApi = 'https://api.stayalive.fr'
 
-export const tokenReportBug = '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'
+export const tokenReportBug =
+  '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'
